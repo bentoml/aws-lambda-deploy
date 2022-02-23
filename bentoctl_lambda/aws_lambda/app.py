073f8d8ae26c4f3a7c593581df1b00620dd81430
Jithin James <jamesjithin97@gmail.com>
2022-02-23 23:39:57 +0530

    disable the bentoml metrics for lambda (#25)

--- bentoctl_lambda/aws_lambda/app.py
+++ bentoctl_lambda/aws_lambda/app.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from bentoml import load
+from bentoml._internal.configuration.containers import DeploymentContainer
 from mangum import Mangum
 
 api_name = os.environ["BENTOML_API_NAME"]
@@ -12,4 +13,7 @@ print("bento service", bento_service)
 # bento_service_api = bento_service.get_inference_api(api_name)
 
 this_module = sys.modules[__name__]
+# Disable /metrics endpoint since promethues is not configured for use
+# in lambda
+DeploymentContainer.api_server_config.metrics.enabled.set(False)
 setattr(this_module, api_name, Mangum(bento_service.asgi_app))
