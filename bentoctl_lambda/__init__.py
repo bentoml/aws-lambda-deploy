4551965c46db580a1e6c3b003d50f9aa06d87b94
jjmachan <jamesjithin97@gmail.com>
2022-03-27 19:11:33 +0530

    added basic functionalities

--- bentoctl_lambda/__init__.py
+++ bentoctl_lambda/__init__.py
@@ -1,4 +1,5 @@
 from .generate import generate
-from .create_deployabe import create_deployabe
+from .create_deployable import create_deployable
+from .registry_utils import get_default_registry_creds
 
-__all__ = ["create_deployabe","generate"]
+__all__ = ["create_deployable", "generate", "get_default_registry_creds"]
