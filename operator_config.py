5621f2e72fb075e2c678882b0a54b57a9950b0d5
jjmachan <jamesjithin97@gmail.com>
2022-03-27 19:11:55 +0530

    moved schema to new file

--- operator_config.py
+++ operator_config.py
@@ -1,26 +1,5 @@
+from bentoctl_lambda.schemas import OPERATOR_SCHEMA
+
 OPERATOR_NAME = "aws-lambda"
 
 OPERATOR_MODULE = "bentoctl_lambda"
-
-OPERATOR_SCHEMA = {
-    "region": {
-        "required": True,
-        "type": "string",
-        "default": "us-west-1",
-        "help_message": "AWS region for Lambda deployment",
-    },
-    "timeout": {
-        "required": False,
-        "type": "integer",
-        "coerce": int,
-        "default": 10,
-        "help_message": "Timeout per request",
-    },
-    "memory_size": {
-        "required": False,
-        "type": "integer",
-        "coerce": int,
-        "default": 512,
-        "help_message": "The memory for your function, set a value between 128 MB and 10,240 MB in 1-MB increments",
-    },
-}
