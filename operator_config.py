99dfe339766189cb4de444d28013a50b28a477b7
Bozhao <yubz86@gmail.com>
2022-04-11 17:00:29 -0700

    Update operator_config.py

--- operator_config.py
+++ operator_config.py
@@ -3,3 +3,7 @@ from bentoctl_lambda.schemas import OPERATOR_SCHEMA
 OPERATOR_NAME = "aws-lambda"
 
 OPERATOR_MODULE = "bentoctl_lambda"
+
+OPERATOR_DEFAULT_TEMPLATE = "terraform"
+
+OPERATOR_AVAILABLE_TEMPLATES = ["terraform"]
