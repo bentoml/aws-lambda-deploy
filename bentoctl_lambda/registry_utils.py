3feb9ad38e655afe7455481983bcd9e41fb268e9
jjmachan <jamesjithin97@gmail.com>
2022-04-21 01:18:17 +0530

    feat: new functionalities to create and delete repository

--- bentoctl_lambda/registry_utils.py
+++ bentoctl_lambda/registry_utils.py
@@ -16,12 +16,18 @@ def get_ecr_login_info(region, repository_id):
     return registry_url, username, password
 
 
+def get_repository(ecr_client, repository_name):
+    result = ecr_client.describe_repositories(repositoryNames=[repository_name])
+    repository_id = result["repositories"][0]["registryId"]
+    repository_uri = result["repositories"][0]["repositoryUri"]
+
+    return repository_id, repository_uri
+
+
 def create_ecr_repository_if_not_exists(region, repository_name):
     ecr_client = boto3.client("ecr", region)
     try:
-        result = ecr_client.describe_repositories(repositoryNames=[repository_name])
-        repository_id = result["repositories"][0]["registryId"]
-        repository_uri = result["repositories"][0]["repositoryUri"]
+        repository_id, repository_uri = get_repository(ecr_client, repository_name)
     except ecr_client.exceptions.RepositoryNotFoundException:
         result = ecr_client.create_repository(repositoryName=repository_name)
         repository_id = result["repository"]["registryId"]
@@ -29,13 +35,25 @@ def create_ecr_repository_if_not_exists(region, repository_name):
     return repository_id, repository_uri
 
 
-def get_registry_info(deployment_name, operator_spec):
+def create_repository(repository_name, operator_spec):
     """
-    Return registry info
+    Create the ECR registry and return the info.
     """
     repo_id, _ = create_ecr_repository_if_not_exists(
-        operator_spec["region"], deployment_name
+        operator_spec.get("region"), repository_name
     )
     repo_url, username, password = get_ecr_login_info(operator_spec["region"], repo_id)
 
     return repo_url, username, password
+
+
+def delete_repository(repository_name, operator_spec):
+    """
+    Destroy the ECR registry created.
+    """
+    ecr_client = boto3.client("ecr", operator_spec.get("region"))
+    try:
+        get_repository(ecr_client, repository_name)
+        ecr_client.delete_repository(repositoryName=repository_name, force=True)
+    except ecr_client.exceptions.RepositoryNotFoundException:
+        print(f"Repository {repository_name} not found. Skipping registry cleanup")
