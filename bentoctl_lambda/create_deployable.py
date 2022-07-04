5c140085f85a4b78701c9c5fd8cb09e6b191994e
Jithin James <jamesjithin97@gmail.com>
2022-07-04 09:42:09 +0530

    feat: use bentoml config to disable prometheus (#44)
    * test lambda locally
    
    * use bentoml_config file
    
    * change entrypoint string
    
    * add docs for dev
    
    * update bentoml api_server config

--- bentoctl_lambda/create_deployable.py
+++ bentoctl_lambda/create_deployable.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 
 import os
 import shutil
-from sys import version_info
 from pathlib import Path
-from attr import asdict
+from sys import version_info
 from typing import Any
+
+from attr import asdict
 
 if version_info >= (3, 8):
     from shutil import copytree
@@ -19,6 +20,9 @@ from bentoml._internal.bento.gen import generate_dockerfile
 LAMBDA_DIR = Path(os.path.dirname(__file__), "aws_lambda")
 TEMPLATE_PATH = LAMBDA_DIR.joinpath("template.j2")
 APP_PATH = LAMBDA_DIR.joinpath("app.py")
+ENTRY_SCRIPT = LAMBDA_DIR.joinpath("entry_script.sh")
+AWS_LAMBDA_RIE = LAMBDA_DIR.joinpath("aws-lambda-rie-x86")
+BENTOML_CONFIG_FILE = LAMBDA_DIR.joinpath("bentoml_server_config.yaml")
 
 
 def create_deployable(
@@ -49,11 +53,6 @@ def create_deployable(
     deployable_path = Path(destination_dir)
     copytree(bento_path, deployable_path, dirs_exist_ok=True)
 
-    if deployable_path.exists():
-        if not overwrite_deployable:
-            print("Using existing deployable")
-            return str(deployable_path)
-
     bento_metafile = Path(bento_path, "bento.yaml")
     with bento_metafile.open("r", encoding="utf-8") as metafile:
         info = BentoInfo.from_yaml_file(metafile)
@@ -63,15 +62,32 @@ def create_deployable(
 
     dockerfile_path = deployable_path.joinpath("env", "docker", "Dockerfile")
     with dockerfile_path.open("w", encoding="utf-8") as dockerfile:
-        dockerfile.write(
-            generate_dockerfile(
-                DockerOptions(**options).with_defaults(),
-                str(deployable_path),
-                use_conda=not info.conda.is_empty(),
-            )
+        dockerfile_generated = generate_dockerfile(
+            DockerOptions(**options).with_defaults(),
+            str(deployable_path),
+            use_conda=not info.conda.is_empty(),
         )
+        dockerfile.write(dockerfile_generated)
 
     # copy over app.py file
     shutil.copy(str(APP_PATH), os.path.join(deployable_path, "app.py"))
+
+    # the entry_script.sh file that will be the entrypoint
+    shutil.copy(
+        str(ENTRY_SCRIPT),
+        os.path.join(deployable_path, "env", "docker", "entry_script.sh"),
+    )
+
+    # aws-lambda runtime-interface-emulator - check docs for more info
+    shutil.copy(
+        str(AWS_LAMBDA_RIE),
+        os.path.join(deployable_path, "aws-lambda-rie"),
+    )
+
+    # bentoml_config_file to dissable /metrics
+    shutil.copy(
+        str(BENTOML_CONFIG_FILE),
+        os.path.join(deployable_path, "bentoml_config.yaml"),
+    )
 
     return str(deployable_path)
