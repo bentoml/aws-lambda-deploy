ee4b31cf73e8e3a0eff286847dddc677a2eb7999
Jithin James <jamesjithin97@gmail.com>
2022-07-20 19:57:50 +0530

    fix: using standalone_load flag (#46)

--- bentoctl_lambda/aws_lambda/app.py
+++ bentoctl_lambda/aws_lambda/app.py
@@ -4,8 +4,10 @@ from bentoml import load
 from mangum import Mangum
 
 API_GATEWAY_STAGE = os.environ.get("API_GATEWAY_STAGE", None)
+
 print("Loading from dir...")
-bento_service = load("./")
+bento_service = load("./", standalone_load=True)
+
 print("bento service", bento_service)
 
 mangum_app = Mangum(bento_service.asgi_app, api_gateway_base_path=API_GATEWAY_STAGE)
